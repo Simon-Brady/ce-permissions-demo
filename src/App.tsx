301831d8cc44cf7a94391945762f883804101964
Simon-Brady <sbrady@consumer-edge.com>
2022-03-25 13:10:10 -0400

    style: nicer! add background color and remove margin

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,39 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader/root';
 import { Navbar } from './components/Navbar';
+import styled from '@emotion/styled';
+
+const StyledWrapper = styled.div`
+	padding: 0 8px 12px;
+`;
+
 const App = () => {
 	return (
 		<>
 			<Navbar />
-			<div>
-				<h1>Permissions</h1>
+			<StyledWrapper>
 				<div>
-					<b>permissions container</b>
+					<h1>Permissions</h1>
 					<div>
-						<h3>Roles container</h3>
-						<div>Role</div>
-						<div>Create Role Button</div>
-					</div>
-					<div>
-						<h3>Tags Container</h3>
-						<div>Tag</div>
-						<div>Create Tag Group</div>
+						<b>permissions container</b>
+						<div>
+							<h3>Roles container</h3>
+							<div>Role</div>
+							<div>Create Role Button</div>
+						</div>
+						<div>
+							<h3>Tags Container</h3>
+							<div>Tag</div>
+							<div>Create Tag Group</div>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div>
-				<h1>Users</h1>
-				<div>User</div>
-				<div>Add User Buttton</div>
-			</div>
+				<div>
+					<h1>Users</h1>
+					<div>User</div>
+					<div>Add User Buttton</div>
+				</div>
+			</StyledWrapper>
 		</>
 	);
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Logo from '../../../assets/CE-Logo-.svg';
-const StyledNav = styled.nav``;
+const StyledNav = styled.nav`
+	background: black;
+	padding: 8px 12px;
+`;
 const Navbar = () => {
 	return (
 		<StyledNav>
