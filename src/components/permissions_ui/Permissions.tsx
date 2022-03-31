03fa30305e99b0f7d5122b1c93a4abd8ea9f34a2
Simon-Brady <sbrady@consumer-edge.com>
2022-03-31 10:16:39 -0400

    chore: point before data restructure

--- src/components/permissions_ui/Permissions.tsx
+++ src/components/permissions_ui/Permissions.tsx
@@ -54,7 +54,7 @@ const Permissions = () => {
 							<h3>Role Permissions</h3>
 							{Object.entries(roles).map(([key, val], i) => {
 								console.log(roles);
-								console.log('key', key);
+								console.log('key', key, val);
 								let tagNameNames = val.applied_tags_ids.map((id, y) => {
 									console.log('id', id);
 									return (
